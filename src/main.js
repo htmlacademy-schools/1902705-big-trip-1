5e8271000a412a3ec6a9e1d1e37a4c13923dfd4d
Mireayo <mireayo.t@gmail.com>
2022-03-03 16:53:59 +0500

    Разбиение страницы на компоненты

---
+++ src/main.js
@@ -0,0 +1,34 @@
+import {createSiteInfoTemplate} from './view/site-info-view';
+import {createSiteMenuTemplate} from './view/site-menu-view';
+import {createFiltersTemplate} from './view/filters-view';
+import {createSortTemplate} from './view/sort-view';
+import {createEditFormTemplate} from './view/edit-form-view';
+import {createWaypointTemplate} from './view/waypoint-view';
+
+export const RenderPosition = {
+  BEFOREBEGIN: 'beforebegin',
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+  AFTEREND: 'afterend',
+};
+
+export const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const tripMainElement = document.querySelector('.trip-main');
+const tripNavigationElement = tripMainElement.querySelector('.trip-controls__navigation');
+const tripFiltersElement = tripMainElement.querySelector('.trip-controls__filters');
+const tripEventsElement = document.querySelector('.trip-events');
+const tripEventsListElement = document.querySelector('.trip-events__list');
+
+
+renderTemplate(tripMainElement, createSiteInfoTemplate(), RenderPosition.AFTERBEGIN);
+renderTemplate(tripNavigationElement, createSiteMenuTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(tripFiltersElement, createFiltersTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(tripEventsElement, createSortTemplate(), RenderPosition.AFTERBEGIN);
+renderTemplate(tripEventsListElement, createEditFormTemplate(), RenderPosition.BEFOREEND);
+
+for (let i = 0; i < 3; i++){
+  renderTemplate(tripEventsListElement, createWaypointTemplate(), RenderPosition.BEFOREEND);
+}
