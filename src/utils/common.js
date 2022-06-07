8e509ebd588f26af0b5e4eda9ca20559eb0039d7
Mireayo <mireayo.t@gmail.com>
2022-06-07 22:38:30 +0500

    Добавлен flatpickr

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,9 +1,10 @@
-export const getRandomInt = (a = 0, b = 1) => {
-  const lower = Math.ceil(Math.min(a,b));
+export const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
 
-  return Math.floor(lower+Math.random() * (upper - lower +1));
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
+
 
 export const updateItem = (items, update) => {
   const index = items.findIndex((item) => item.id === update.id);
