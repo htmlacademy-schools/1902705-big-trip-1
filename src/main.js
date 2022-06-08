7d66e74a7645e117fecdf4869b5697bbd43603f1
Mireayo <mireayo.t@gmail.com>
2022-06-08 21:36:16 +0500

    7.1.2 Работают фильтры, удаление и добавление точек, но есть баги в фильтрации

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
-import SiteInfoView from './view/site-info-view';
 import SiteMenuView from './view/site-menu-view';
-import FiltersView from './view/filters-view';
-import { render, renderPosition } from './utils/render';
+import { render, renderPosition} from './utils/render';
 import { generatePoint } from './mock/point';
-import TripPresenter from './presenter/Trip-presenter';
-import PointsModel from './model/points-model';
+import TripPresenter from './presenter/trip-presenter';
+import PointModel from './model/point-model';
+import FilterModel from './model/filter-model';
 
-
-const POINT_COUNT = 15;
+const POINT_COUNT = 5;
 const points = Array.from({ length: POINT_COUNT }, generatePoint);
 
 const tripBody = document.querySelector('.page-body');
@@ -15,18 +13,19 @@ const headerMenu = tripBody.querySelector('.trip-main');
 const siteMenuElement = tripBody.querySelector('.trip-controls__navigation');
 const filtersElement = tripBody.querySelector('.trip-controls__filters');
 const mainContainer = tripBody.querySelector('.trip-events');
-const pointsModel = new PointsModel();
-pointsModel.points = points;
 
+const pointModel = new PointModel();
+const filterModel = new FilterModel();
 
-const tripPresenter = new TripPresenter(mainContainer, pointsModel);
+pointModel.points = points;
 
-if (points.length !== 0) {
-  render(headerMenu, new SiteInfoView(points[0]).element, renderPosition.AFTERBEGIN);
-}
+const tripPresenter = new TripPresenter(mainContainer, pointModel, headerMenu, filterModel, filtersElement);
 
 render(siteMenuElement, new SiteMenuView(), renderPosition.BEFOREEND);
-render(filtersElement, new FiltersView(), renderPosition.BEFOREEND);
+tripPresenter.init(pointModel.points);
 
+document.querySelector('.trip-main__event-add-btn').addEventListener('click', (evt) => {
+  evt.preventDefault();
+  tripPresenter.createPoint();
+});
 
-tripPresenter.init();
